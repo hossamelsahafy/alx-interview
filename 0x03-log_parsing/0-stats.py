a3b37a38bd0045e2882ffab3391cb2f26189268d
hossamelsahafy <hossamelsa7afy54@gmail.com>
2024-06-21 01:51:50 +0300

    File

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,37 +1,36 @@
 #!/usr/bin/python3
 """
-    Write a script that reads stdin
-    line by line and computes metrics
+    Log parsing
 """
 import sys
-
-
-import sys
 import re
-from collections import defaultdict
 
-total_size = 0
-status_counts = defaultdict(int)
+file_size = 0
+status_codes = {}
 
-line_count = 0
 try:
-    for line in sys.stdin:
-        line_count += 1
-        match = re.match(r'^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) - \[(.*)\] "GET (.*) HTTP/1\.1" (\d+) (\d+)$', line)
+    for i, line in enumerate(sys.stdin):
+        pattern = (
+            r"^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3} - \[.*\] "
+            r"\"GET /projects/260 HTTP/1.1\" (\d+) (\d+)$"
+        )
+
+        match = re.match(pattern, line)
+
         if match:
-            status_code = int(match.group(4))
-            file_size = int(match.group(5))
-            total_size += file_size
-            status_counts[status_code] += 1
-            if line_count % 10 == 0:
-                print("Total file size: {}".format(total_size))
-                print("Number of lines by status code:")
-                for status_code in sorted(status_counts.keys()):
-                    print("{}: {}".format(status_code, status_counts[status_code]))
-                print()
+            file_size += int(match.group(2))
+            status_code = int(match.group(1))
+            if status_code in status_codes:
+                status_codes[status_code] += 1
+            else:
+                status_codes[status_code] = 1
+
+        if (i + 1) % 10 == 0:
+            print("File size:", file_size)
+            for status_code in sorted(status_codes.keys()):
+                print(f"{status_code}: {status_codes[status_code]}")
+
 except KeyboardInterrupt:
-    print("Total file size: {}".format(total_size))
-    print("Number of lines by status code:")
-    for status_code in sorted(status_counts.keys()):
-        print("{}: {}".format(status_code, status_counts[status_code]))
-    print()
+    print("File size:", file_size)
+    for status_code in sorted(status_codes.keys()):
+        print(f"{status_code}: {status_codes[status_code]}")
