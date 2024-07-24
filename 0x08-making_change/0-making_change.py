d67bad8537043c6ad31939f6ffb0df946d06f4ea
hossamelsahafy <hossamelsa7afy54@gmail.com>
2024-07-24 20:22:33 +0300

    File

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
-"""Change comes from within"""
+"""Change making module.
+"""
 
 
-def makeChange(coins, total) -> int:
-    """Make Change Method"""
+def makeChange(coins, total):
     if total <= 0:
         return 0
 
-    dp = [float('inf')] * (total + 1)
-    dp[0] = 0
+    min_coins = [float('inf')] * (total + 1)
+    min_coins[0] = 0
 
     for coin in coins:
-        for amount in range(coin, total + 1):
-            dp[amount] = min(dp[amount], dp[amount - coin] + 1)
+        for i in range(coin, total + 1):
+            min_coins[i] = min(min_coins[i], min_coins[i - coin] + 1)
 
-    return dp[total] if dp[total] != float('inf') else -1
+    return min_coins[total] if min_coins[total] != float('inf') else -1
