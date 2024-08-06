e8345adb6e2f3eb139dc019ec1d6c44d91a2328f
hossamelsahafy <hossamelsa7afy54@gmail.com>
2024-08-06 10:16:52 +0300

    File

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -5,48 +5,36 @@
 
 
 def isWinner(x, nums):
-    """Is Winner Method"""
+    """Determine the winner of the Prime Game"""
     if x <= 0 or not nums:
         return None
+    if x != len(nums):
+        return None
 
-    def sieve(n):
-        """Sieve Method"""
-        is_prime = [True] * (n + 1)
-        p = 2
-        while (p * p <= n):
-            if (is_prime[p]):
-                for i in range(p * p, n + 1, p):
-                    is_prime[i] = False
-            p += 1
-        is_prime[0], is_prime[1] = False, False
-        return [p for p in range(n + 1) if is_prime[p]]
+    ben = 0
+    maria = 0
 
     max_num = max(nums)
-    primes = sieve(max_num)
+    is_prime = [True] * (max_num + 1)
+    is_prime[0], is_prime[1] = False, False
 
-    maria_wins = 0
-    ben_wins = 0
+    for i in range(2, int(max_num**0.5) + 1):
+        if is_prime[i]:
+            for j in range(i * i, max_num + 1, i):
+                is_prime[j] = False
+
+    prime_count = [0] * (max_num + 1)
+    for i in range(1, max_num + 1):
+        prime_count[i] = prime_count[i - 1] + (1 if is_prime[i] else 0)
 
     for n in nums:
-        primes_in_game = [p for p in primes if p <= n]
-        if not primes_in_game:
-            ben_wins += 1
-            continue
-
-        turn = 0
-        while primes_in_game:
-            prime = primes_in_game.pop(0)
-            primes_in_game = [p for p in primes_in_game if p % prime != 0]
-            turn += 1
-
-        if turn % 2 == 1:
-            maria_wins += 1
+        if prime_count[n] % 2 == 0:
+            ben += 1
         else:
-            ben_wins += 1
+            maria += 1
 
-    if maria_wins > ben_wins:
-        return "Maria"
-    elif ben_wins > maria_wins:
+    if ben > maria:
         return "Ben"
-    else:
-        return None
+    if maria > ben:
+        return "Maria"
+    return None
