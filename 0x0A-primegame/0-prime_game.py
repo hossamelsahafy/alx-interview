6b3239d938ca2649082cad631c204038d02b0b0e
hossamelsahafy <hossamelsa7afy54@gmail.com>
2024-08-06 10:13:08 +0300

    File

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -14,7 +14,7 @@ def isWinner(x, nums):
         is_prime = [True] * (n + 1)
         p = 2
         while (p * p <= n):
-            if (is_prime[p] == True):
+            if (is_prime[p]):
                 for i in range(p * p, n + 1, p):
                     is_prime[i] = False
             p += 1
